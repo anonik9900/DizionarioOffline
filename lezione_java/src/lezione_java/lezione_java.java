736e58569128df19bc8b9657eb16dc438c5de840
DeVeL_AnOnIk <impieri.nicholas@gmail.com>
2017-10-18 18:27:10 +0200

    Add files via upload

---
+++ lezione_java/src/lezione_java/lezione_java.java
@@ -0,0 +1,53 @@
+package lezione_java;
+
+public class lezione_java {
+
+	public static void main(String[] args) {
+		
+	System.out.println("Ciao Mondo");	
+	int a=16,b=5,c;
+	
+	c=a-b;
+	a++;
+	b++; //b=b+1 è una sintassi semplificata incrementa di uno b--; = decremento
+	a+=b; // a=a+b-->a+=5-->a=a+5
+	
+	String nome="Nicholas";
+	
+	//System.out.println (ricorda linux)
+	
+	System.out.println(nome+" ha anni: "+a); //Se si mette l'a++ gli anni aumentano a 17
+	
+	System.out.println("sottrazione "+c);
+	
+	System.out.println("valori : "+a+ " , "+b); //per visualizzare le variabili semplificate
+	
+	//ricorda la sottrazione si effettua dichiarando a=numero b=numero c; e c=a-b
+	
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
